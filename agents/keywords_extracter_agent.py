a18932080542d320e6b2b44653317b783131ebdd
Manasa <yelamarthymanasa@gmail.com>
2025-06-09 13:55:31 +0530

    coded

---
+++ agents/keywords_extracter_agent.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")  
+
+prompt_template = ChatPromptTemplate.from_messages([
+    ("system", 
+     "You are an intelligent research assistant working on building a literature review agent. "
+     "Your task is to extract **5 to 7 complete, meaningful search phrases** from the given research topic. "
+     "These phrases will be used to search for academic papers in tools like Semantic Scholar or arXiv.\n\n"
+     " Each phrase should be:\n"
+     "- Specific and informative\n"
+     "- 5 to 15 words long\n"
+     "- Contain no incomplete terms\n"
+     "- Sound like a research paper title or academic query\n"
+     "- Avoid generic words alone (like just 'AI', 'models', 'data')\n\n"
+     "Do NOT return single words or broken short phrases.\n"
+     " Avoid repetition or overlapping meanings.\n\n"
+     "Return only a comma-separated list of meaningful research phrases."
+    ),
+    ("user", "Extract important research keywords from the following topic: {topic}")
+])
+
+
+chain = prompt_template | model
+result = chain.invoke({
+    "topic": "Predicting Disease Progression Using OCT Data. Predicting prognosis and number of anti VEGF injections needed and visual acuity improvement  in DME, CNVM  based on OCT , Age ,Vision, clinical risk factors."})
+print("Extracted Keywords:", result.content)

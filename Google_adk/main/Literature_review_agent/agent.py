d6700b4eefe83ff085e08404be415da9db00a3e1
Manasa <yelamarthymanasa@gmail.com>
2025-08-16 12:22:29 +0530

    fixed errors in agents

--- Google_adk/main/Literature_review_agent/agent.py
+++ Google_adk/main/Literature_review_agent/agent.py
@@ -8,7 +8,7 @@ from .sub_agents.research_gap import researchGap_agent
 from .sub_agents.conclusion_agent import conclusion_agent
 
 
-root_agent = SequentialAgent(
+literatureReview_agent = SequentialAgent(
     name="LiteratureReviewAgent",
     sub_agents=[
         introduction_agent,
@@ -18,10 +18,8 @@ root_agent = SequentialAgent(
         conclusion_agent
     ],
     description=(
-        " If user wishes you wish him back and ask his for a title untill he provide title dont run all your subagents after then you can run all subagent sequentially"
         "Executes a literature review pipeline :, "
-        "If user havent provided title then only main agent agent should respond not all agents after providing the title then all should work sequentially"
-        "According to  the below order:"
+         "According to  the below order:"
         "1. Introduction,"
         "2.paper search,"
         "3. anaylsis, "
